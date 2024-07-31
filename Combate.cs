4acb10e3afee840c736f6bd609e40c5cd6b6c37d
NatyBlass <natypaz12345@gmail.com>
2024-07-31 15:49:29 -0300

    Mejora en los combates para alargar su duración

--- Combate.cs
+++ Combate.cs
@@ -17,6 +17,11 @@ public class Combate
             {
                 Console.WriteLine($"{jug2.Nombre} ha sido derrotado");
                 MejorarAtributo(jug1);
+
+                if (jug1.Victorias % 3 == 0)  //Agregué el parámetro de Victorias para que cada 3 victorias mi personaje pueda ser mejorado, de lo contrario sería muy desbalanceado que por cada victoria, se pueda mejorar
+                {
+                    MejorarAtributo(jug1);
+                }
                 return jug1;
             }
 
@@ -50,12 +55,34 @@ public class Combate
 
     private int CalcularDaño(GeneracionPersonaje jug)
     {
-        int daño;
+        int dañoBase, dañoACausar; //dañoBase me permite utilizar los atributos de fuerza y poder para obtener un equilibrio entre ambos a la hora de hacer el cálculo del daño a causar
+        Random random = new Random();
+        double varDaño = random.NextDouble() * 0.5 + 0.5; //me permitirá equilibrar el daño entre el 50% y 100% del valor de la fuerza del jugador
+        //random.NextDouble() * 0.5 me da un random entre 0.0 y 0.5, le sumo el otro 0.5 para poder equilibrar esto entre 0.5 y 1.0
 
-        daño = jug.Fuerza + jug.Poder / 2;
-
-        return daño;
+        dañoBase = (int)((jug.Fuerza * 0.5) + (jug.Poder * 0.5)); //combino la suma de la mitad de ambos atributos. | hice el casteo ya que los atributos son "double"
+        dañoACausar = (int)(dañoBase * varDaño); // obtengo el daño a causar a partir del daño base y el porcentaje de variacion de éste, es decir que hará un efecto del 50 al 100%
+        
+        return dañoACausar;
     }
+
+    private bool AtaqueAcertado(GeneracionPersonaje jug)
+    {
+        Random random = new Random();
+        double probabilidadBase;
+        bool ataqueExitoso;
+
+        probabilidadBase = 0.8 + jug.Velocidad * 0.01; //esto determina la probabilidad de que el ataque sea acertado entre un 80% y un 1% más por cada punto de velocidad.
+        if (probabilidadBase > 1.0)
+        {
+            probabilidadBase = 1.0;
+        }
+
+        ataqueExitoso = random.NextDouble() < probabilidadBase; //determina si el ataque es acertado
+
+        return ataqueExitoso;
+    }
+
 
     private void MejorarAtributo(GeneracionPersonaje personaje)
     {
